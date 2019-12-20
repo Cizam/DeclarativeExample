4edb77eb3fbaab9b705eb2fe589137e45c4d550a
Cesar Ivan Zamora Gutierrez <cesar.zamora@globant.com>
2019-12-20 00:14:49 -0600

    First upload

---
+++ ButtonSwiftUI/ButtonSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ButtonSwiftUI
+//
+//  Created by Cesar Ivan Zamora Gutierrez on 12/20/19.
+//  Copyright © 2019 Cesar Ivan Zamora Gutierrez. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
